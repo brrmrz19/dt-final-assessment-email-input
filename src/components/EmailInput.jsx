cfd71e64d1a2da2217bb7157983503acad581e91
brrmrz19 <brrmrz19@gmail.com>
2025-06-07 14:47:07 +0800

    feat/wip: add email input

---
+++ src/components/EmailInput.jsx
@@ -0,0 +1,12 @@
+export default function EmailInput() {
+  return (
+    <div className="flex items-center border-0 bg-[#FDFDFD] border-gray-300 rounded-[8px] w-[400px] h-[49.47px] text-[16px] shadow-2xl">
+      <input
+        type="text"
+        value="lala"
+        placeholder="Enter recipients..."
+        className=" px-4 py-2 outline-none text-black "
+      />
+    </div>
+  );
+}
